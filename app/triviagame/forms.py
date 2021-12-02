63886642cdd43a0053d8cc88f1954d189cc96f4d
Matt Cooper <vtbassmatt@github.com>
2021-12-02 14:47:47 -0500

    remove restriction on team names

--- app/triviagame/forms.py
+++ app/triviagame/forms.py
@@ -11,34 +11,24 @@ class JoinGameForm(forms.Form):
 
 
 class CreateTeamForm(forms.Form):
-    TEAM_NAME_CHOICES = (
-        ("Win, Booze, or Draw", "Win, Booze, or Draw"),
-        ("Red Hot Trivia Peppers", "Red Hot Trivia Peppers"),
-        ("The Smartinis", "The Smartinis"),
-        ("The Scranton Stranglers", "The Scranton Stranglers"),
-        ("Harry, You're a Quizard", "Harry, You're a Quizard"),
-        ("I Can't Feel My Mace When I'm Windu", "I Can't Feel My Mace When I'm Windu"),
-        ("Walt Quizney World", "Walt Quizney World"),
-        ("Bears, Beets, Battlestar Galactica", "Bears, Beets, Battlestar Galactica"),
-        ("Dame Agatha Quiztie", "Dame Agatha Quiztie"),
+    TEAM_NAME_IDEAS = (
+        "Win, Booze, or Draw",
+        "Red Hot Trivia Peppers",
+        "The Smartinis",
+        "The Scranton Stranglers",
+        "Harry, You're a Quizard",
+        "I Can't Feel My Mace When I'm Windu",
+        "Walt Quizney World",
+        "Bears, Beets, Battlestar Galactica",
+        "Dame Agatha Quiztie",
     )
 
-    #name = forms.CharField(label='Team name', max_length=200, widget=Bs5TextInput)
-    name = forms.ChoiceField(
+    name = forms.CharField(
         label='Team name',
-        # team names will be shuffled in __init__, but have to be here for
-        # validation to pass
-        choices=TEAM_NAME_CHOICES,
-        widget=Bs5RadioSelect,
+        max_length=200,
+        widget=Bs5TextInput,
     )
     members = forms.CharField(label='Team members', widget=Bs5Textarea)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['name'].widget.choices = random.sample(
-            CreateTeamForm.TEAM_NAME_CHOICES,
-            k=len(CreateTeamForm.TEAM_NAME_CHOICES),
-        )
 
 
 class ReJoinTeamForm(forms.Form):
