f5216cddfce34940d64bad13f0ba2a8c8607d9c2
Matt Cooper <vtbassmatt@gmail.com>
2022-10-16 10:31:23 -0400

    add htmx

--- triviagame/settings.py
+++ triviagame/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_htmx',  
     'game.apps.GameConfig',
     'host.apps.HostConfig',
     'django.forms',
@@ -63,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'triviagame.urls'
