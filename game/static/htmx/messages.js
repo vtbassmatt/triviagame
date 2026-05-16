1a4f7a7db459b04581554744f3e87b380b20748e
Matt Cooper <vtbassmatt@gmail.com>
2026-05-16 11:15:13 -0400

    htmx.on seems to have changed behavior, and was only a thin wrapper around querySelector() anyhow

--- game/static/htmx/messages.js
+++ game/static/htmx/messages.js
@@ -1,17 +1,17 @@
 function createMessage(message) {
   // clone template
-  const elem = htmx.find("[data-msg-template").content.cloneNode(true);
+  const elem = document.querySelector("[data-msg-template]").content.cloneNode(true);
 
   // update contents
-  const wrapper = htmx.find(elem, "[data-msg-wrapper]");
+  const wrapper = elem.querySelector("[data-msg-wrapper]");
   wrapper.className += " alert-" + message.tags;
-  const body = htmx.find(elem, "[data-msg-body]");
+  const body = elem.querySelector("[data-msg-body]");
   body.innerText = message.message;
 
   // add to page
-  htmx.find("[data-msg-container]").appendChild(elem);
+  document.querySelector("[data-msg-container]").appendChild(elem);
 }
 
 htmx.on("messages", (e) => {
-  e.detail.value.forEach(createMessage);
+  e.detail.forEach(createMessage);
 })
